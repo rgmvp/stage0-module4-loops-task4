21fc46217bc1b2046c36ce24e4086307202575e7
rgmvp <perez4horoneniepwnz@yandex.ru>
2023-02-01 22:51:22 +0300

    done

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
+        int a = 1;
+        for(int i = 1; i <= lastPrinted; i++){
+            if(a % 3 != 0) {
+                System.out.println(i);
+            }
+            a++;
+
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+        if (numberToSkip > lastInRow){
+            System.out.println("number to skip is bugger then the last");
+        }
+        else if (lastInRow < 0){
+            System.out.println("last number in row is negative");
+        }
+        else if ()
 
     }
 }
